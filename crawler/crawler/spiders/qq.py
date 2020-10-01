72d2d483fe1d95955cba9672e2327a82712301f3
CaveSpider <18310139362@163.com>
2020-10-01 19:47:09 +0800

    Change the name of the spider to QqNewsInfo Spider. Add some note. Simplify imported modules

--- crawler/crawler/spiders/qq.py
+++ crawler/crawler/spiders/qq.py
@@ -1,55 +1,4 @@
-'''
-import scrapy
-import re
-from crawler.items import NewsItem
-
-class QqSpider(scrapy.Spider):
-    name = 'qq'
-    allowed_domains = ['qq.com', 'news.qq.com', 'sports.qq.com', 'new.qq.com']
-    start_urls = ['https://news.qq.com/']
-    total_count = 0
-
-    def parse(self, response):
-        if(self.total_count >= 100):
-            return
-        current_url = response.request.url
-        alist = response.xpath('//a/@href').extract()
-        pattern = '.+/omn/2020[0-9]{4}/(2020[0-9]{4}[A-za-z0-9]+).html'
-        match_obj = re.match(pattern, current_url)
-        if(match_obj != None):
-            self.total_count += 1
-            item = NewsItem()
-            item['source'] = self.name
-            title = response.xpath('//h1/text()').extract()
-            item['title'] = title[0]
-            item['news_id'] = match_obj.group(1)
-            year = response.xpath('//div[@class="year through"]/span/text()').extract()[0]
-            md = response.xpath('//div[@class="md"]/text()').extract()[0]
-            date_pattern = '.*"([0-9]{2})".*"([0-9]{2})".*'
-            date_match_obj = re.match(date_pattern, md)
-            month = date_match_obj.group(1)
-            day = date_match_obj.group(2)
-            time = response.xpath('//div[@class="time"]/text()').extract()[0]
-            time_pattern = '.*"([0-9]{2})".*"([0-9]{2})".*'
-            time_match_obj = re.match(time_pattern, time)
-            hour = time_match_obj.group(1)
-            minute = time_match_obj.group(2)
-            pub_date = year+'.'+month+'.'+day+' '+hour+':'+minute
-            item['pub_date'] = pub_date
-            item['content_text'] = response.xpath('//p[class="one-p"]/text()').extract()
-            item['content_picture'] = response.xapth('//img[class="content-picture"]/@scr').extract()
-            yield item
-        for href in alist:
-            try:
-                yield scrapy.Request(href, callback=self.parse)
-            except:
-                pass
-'''
-
-# CrawlSpider与Rule配合使用可以起到历遍全站的作用、Request干啥的我就不解释了
-from scrapy.spiders import CrawlSpider, Rule, Request
-import scrapy
-# 配合Rule进行URL规则匹配
+from scrapy.spiders import CrawlSpider, Rule, Request, Spider
 from scrapy.linkextractors import LinkExtractor
 import re
 from crawler.items import NewsItem
@@ -94,12 +43,10 @@ class QqIncSpider(CrawlSpider):
     def follow(self, response):
         pass
 
-class QqNewsBriefInfoSpider(scrapy.Spider):
-    name = 'qq_news_brief_info'
+class QqNewsInfoSpider(Spider):
+    name = 'qq_news_info'
     allowed_domains = ['i.news.qq.com', 'new.qq.com']
-    # https://i.news.qq.com/trpc.qqnews_web.kv_srv.kv_srv_http_proxy/list?sub_srv_id=24hours
-    # &srv_id=pc&offset=0&limit=20&strategy=1&ext={%22pool%22:[%22high%22,%22top%22],%22is_filter%22:10,%22check_type%22:true}
-    # 一下是腾讯新闻的不同新闻标签
+    # 以下是腾讯新闻的不同新闻标签
     sub_srv_id_list = ['24hours', 'antip', 'bj', 'ent', 'milite', 'world', 'tech',
                        'finance', 'auto', 'fashion', 'photo', 'games', 'cul', 'finance_stock',
                        'house', 'comic', 'emotion', 'digi', 'astro', 'health', 'visit', 'baby',
@@ -117,6 +64,7 @@ class QqNewsBriefInfoSpider(scrapy.Spider):
     total_error = 0
 
     def parse(self, response):
+        # 爬取新闻信息列表，提取新闻的简要信息，存入data/news_brief_info/文件夹
         try:
             list = json.loads(response.text)['data']['list']
         except:
@@ -135,6 +83,7 @@ class QqNewsBriefInfoSpider(scrapy.Spider):
 
 
     def parse_item(self, response):
+        # 从新闻页爬取信息，存入data/news_info/文件夹
         current_url = response.request.url
         script_list = response.xpath('/html/head//script/text()').extract()
         news_brief_info = None
@@ -161,6 +110,7 @@ class QqNewsBriefInfoSpider(scrapy.Spider):
                     item['content'] += ['text_' + text for text in one_p.xpath('./text()').extract()]
             yield item
         except:
+            # 如果出现错误，将出现错误的url追加存入error/error_url.txt文件
             error_f = open('data/error/error_url.txt', 'a', encoding='utf-8')
             error_f.write(str(self.total_error) + '_' + current_url + '\n')
             error_f.close()

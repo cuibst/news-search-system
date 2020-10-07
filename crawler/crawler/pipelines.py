4d5519da463473a82087cc81f248c925aaa86287
CaveSpider <18310139362@163.com>
2020-10-07 15:03:31 +0800

    Modify incremental spider qq_inc, which crawls newly published news in qq.com index page.

--- crawler/crawler/pipelines.py
+++ crawler/crawler/pipelines.py
@@ -7,11 +7,29 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 import json
+import os
 
 class NewsPipeline:
+    file_set = set()
+    dir_path = ''
+
+    def open_spider(self, spider):
+        # 确定要保存的文件夹路径
+        if (spider.name == 'qq_inc'):
+            self.dir_path = 'data/debug/'
+        elif (spider.name == 'qq_news_info'):
+            self.dir_path = 'data/news_info'
+        # 获取文件夹中的所有文件
+        self.file_set = set(os.listdir(self.dir_path))
+
     def process_item(self, item, spider):
-        f = open('data/news_info/' + item['news_id'] + '.json', 'w', encoding="utf-8")
-        content = json.dumps(dict(item), indent=4, ensure_ascii=False)
-        f.write(content)
-        f.close()
-        return item
+        current_file_name = item['news_id'] + '.json'
+        if(current_file_name not in self.file_set):
+            self.file_set.add(current_file_name)
+            f = open(self.dir_path + current_file_name, 'w', encoding="utf-8")
+            content = json.dumps(dict(item), indent=4, ensure_ascii=False)
+            f.write(content)
+            f.close()
+            return item
+        else:
+            return
--- crawler/crawler/spiders/qq.py
+++ crawler/crawler/spiders/qq.py
@@ -1,10 +1,24 @@
 from scrapy.spiders import CrawlSpider, Rule, Request, Spider
 from scrapy.linkextractors import LinkExtractor
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.log import configure_logging
+from scrapy.utils.project import get_project_settings
+from twisted.internet import reactor, defer
+import time
 import re
+
+# 导包包含上级目录
+import sys
+import os
+fpath = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+ffpath = os.path.abspath(os.path.join(fpath, ".."))
+print(ffpath)
+sys.path.append(ffpath)
+
 from crawler.items import NewsItem
 import json
 from selenium import webdriver
-
+'''
 class QqIncSpider(CrawlSpider):
     # 这是一个功能及其不完善的增量爬虫，使用chromedirver对首页进行爬取
     name = 'qq_inc'
@@ -42,6 +56,58 @@ class QqIncSpider(CrawlSpider):
 
     def follow(self, response):
         pass
+'''
+
+
+class QqIncSpider(Spider):
+    name = "qq_inc"
+    allowed_domains = ['news.qq.com', 'new.qq.com']
+    start_urls = ['https://www.qq.com/']
+    total_error = 0
+
+    def parse(self, response):
+        href_list = response.xpath('//a/@href').extract()
+        pattern = '.+/omn/2020[0-9]{4}/(2020[0-9]{4}[A-za-z0-9]+).*'
+        for href in href_list:
+            if(re.match(pattern, href) != None):
+                yield Request(href, callback=self.parse_item)
+
+
+    def parse_item(self, response):
+        # 从新闻页爬取信息，存入data/news_info/文件夹
+        current_url = response.request.url
+        script_list = response.xpath('/html/head//script/text()').extract()
+        news_brief_info = None
+        for script in script_list:
+            if (script.find('window.DATA = ') >= 0):
+                news_brief_info = json.loads(script.lstrip('window.DATA = '))
+                break
+        item = NewsItem()
+        try:
+            item['title'] = news_brief_info['title']
+            item['category'] = news_brief_info['catalog1']
+            item['media'] = news_brief_info['media']
+            item['pub_date'] = news_brief_info['pubtime']
+            item['tags'] = news_brief_info['tags']
+            item['news_id'] = news_brief_info['cms_id']
+            item['source'] = self.name.split('_')[0]
+            item['news_url'] = current_url
+            item['content'] = []
+            for one_p in response.xpath('//p[@class="one-p"]'):
+                img = one_p.xpath('.//img/@src').extract()
+                if (len(img) != 0):
+                    item['content'] += ['img_' + src for src in img]
+                else:
+                    item['content'] += ['text_' + text for text in one_p.xpath('./text()').extract()]
+            yield item
+        except:
+            # 如果出现错误，将出现错误的url追加存入error/error_url.txt文件
+            error_f = open('data/error/error_url.txt', 'a', encoding='utf-8')
+            error_f.write(str(self.total_error) + '_' + current_url + '\n')
+            error_f.close()
+            self.total_error += 1
+
+
 
 class QqNewsInfoSpider(Spider):
     name = 'qq_news_info'
@@ -117,4 +183,17 @@ class QqNewsInfoSpider(Spider):
             self.total_error += 1
 
 
+runner = CrawlerRunner(get_project_settings())
+configure_logging()
+
+@defer.inlineCallbacks
+def qq_inc_crawl():
+    while True:
+        print('qq_inc spider started')
+        yield runner.crawl(QqIncSpider)
+        time.sleep(30)
+    reactor.stop()
+
+qq_inc_crawl()
+reactor.run()
 

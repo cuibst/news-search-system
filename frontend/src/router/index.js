98519f3f922b210e6de61f443c4a7bccbd804095
hjq <huajq19@mails.tsinghua.edu.cn>
2020-10-07 13:51:45 +0800

    recompose the login component to the loginpage view

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Sample from '../components/Sample.vue'
-import Login from '../components/Login.vue'
+import LoginPage from '../views/LoginPage.vue'
 import Register from '../components/Register.vue'
 import '../assets/styles/global.css'
 
@@ -10,7 +10,11 @@ Vue.use(VueRouter)
 const routes = [
   { path: '/', redirect: '/sample' },
   { path: '/sample', component: Sample },
-  { path: '/login', component: Login },
+  {
+    path: '/login',
+    name: 'LoginPage',
+    component: LoginPage
+  },
   { path: '/register', component: Register }
 ]
 

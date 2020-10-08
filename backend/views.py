fee172f2f83d7f91c211b791547cebe6403987a7
wwh <wuwh19@mails.tsinghua.edu.cn>
2020-10-08 18:47:15 +0800

    add frontend part

--- backend/views.py
+++ backend/views.py
@@ -2,6 +2,8 @@
 views for backend
 '''
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import User
 
 # Create your views here.
 
@@ -12,3 +14,37 @@ def index(request):
     mes = dict()
     mes['message'] = "Hello Software Engineering!"
     return JsonResponse(mes)
+
+@csrf_exempt
+def login(request):
+    '''
+    login
+    '''
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        password = request.POST.get('password')
+        print(name, password)
+        user = User.objects.filter(name=name).first()
+        if not user:
+            return JsonResponse({
+                'code': 401,
+                'data': "invalid user",
+                'token': 'WA1'
+            }, status=200)
+        password0 = user.password
+        if password0 == password:
+            return JsonResponse({
+                'code': 200,
+                'data': 'login successfully',
+                'token': 'AC'
+            }, status=200)
+        return JsonResponse({
+            'code': 401,
+            'data': 'wrong password',
+            'token': 'WA2'
+        }, status=200)
+    return JsonResponse({
+        'code': 405,
+        'data': 'invalid method',
+        'token': 'WA3'
+    }, status=200)

922f5c23b7cf6e61c2e4cc3cb3be68e91fdc41c9
hjq <huajq19@mails.tsinghua.edu.cn>
2020-10-10 14:29:09 +0800

    beautify the register component with the method from element-ui

--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -6,45 +6,33 @@
         <img src="../assets/logo.png">
       </div>
       <!-- 表单 -->
-      <el-form class="login-form">
+      <el-form status-icon :model="ruleForm" :rules="rules" ref="ruleForm" class="login-form">
         <!--用户名-->
-        <el-form-item>
-          <el-input class="input" prefix-icon="el-icon-user" v-model.lazy="username" placeholder="请输入您的用户名">
-            <i class="el-icon-check" slot="append" v-if="username_valid"></i>
-            <!-- 当输入不为空且不和要求时反馈为错误符号 -->
-            <i class="el-icon-close" slot="append" v-if="username_valid===false && username_input===true"></i>
-          </el-input>
+        <el-form-item prop = 'username'>
+          <el-input class="input" prefix-icon="el-icon-user" v-model="ruleForm.username" placeholder="请输入您的用户名"></el-input>
         </el-form-item>
         <!--密码-->
-        <el-form-item>
-          <el-input class="input" prefix-icon="el-icon-view" show-password v-model="password" placeholder="请输入您的密码">
-            <i class="el-icon-check" slot="append" v-if="passwordcheck"></i>
-          </el-input>
+        <el-form-item prop = 'password'>
+          <el-input class="input" prefix-icon="el-icon-view" show-password v-model="ruleForm.password" placeholder="请输入您的密码"></el-input>
         </el-form-item>
         <!--确认密码-->
-        <el-form-item>
-          <el-input class="input" prefix-icon="el-icon-view" show-password v-model="secondpassword" placeholder="请确认您的密码">
+        <el-form-item prop = 'passwordCheck'>
+          <el-input class="input" prefix-icon="el-icon-view" show-password v-model="ruleForm.passwordCheck" placeholder="请确认您的密码">
             <i class="el-icon-check" slot="append" v-if="passwordcheck"></i>
           </el-input>
           <span v-if="passwordcheck===false && password_input" style="color: red;font-size:12px;" class="errorPassword">请输入相同密码!</span>
         </el-form-item>
         <!--邮箱-->
-        <el-form-item>
-          <el-input class="input" prefix-icon="el-icon-s-promotion" v-model="email" placeholder="请输入您的邮箱">
-            <i class="el-icon-check" slot="append" v-if="email_valid"></i>
-            <i class="el-icon-close" slot="append" v-if="email_valid===false && email_input"></i>
-          </el-input>
+        <el-form-item prop = 'email'>
+          <el-input class="input" prefix-icon="el-icon-s-promotion" v-model="ruleForm.email" placeholder="请输入您的邮箱"></el-input>
         </el-form-item>
         <!--手机号-->
-        <el-form-item>
-          <el-input class="input" prefix-icon="el-icon-phone" v-model="phonenumber" placeholder="请输入您的手机号">
-            <i class="el-icon-check" slot="append" v-if="phonenumber_valid"></i>
-            <i class="el-icon-close" slot="append" v-if="phonenumber_valid===false && phonenumber_input"></i>
-          </el-input>
+        <el-form-item prop = 'phonenumber'>
+          <el-input class="input" prefix-icon="el-icon-phone" v-model="ruleForm.phonenumber" placeholder="请输入您的手机号"></el-input>
         </el-form-item>
         <!--按钮-->
         <el-form-item class="logbtn">
-          <el-button type="primary" :disabled="phonenumber_valid === false">注册</el-button>
+          <el-button type="primary" @click="submitForm('ruleForm')">注册</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -55,58 +43,72 @@
 export default {
   name: 'Register',
   data () {
+    var validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请再次输入密码'))
+      } else if (value !== this.ruleForm.password) {
+        callback(new Error('两次输入密码不一致!'))
+      } else {
+        callback()
+      }
+    }
+    var validateEmail = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('邮箱不能为空'))
+      } else if (!(/^([a-zA-Z0-9]+[_|_|.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|_|.]?)*[a-zA-Z0-9]+.[a-zA-Z]{2,3}$/.test(value))) {
+        callback(new Error('请输入正确的邮箱格式!'))
+      } else {
+        callback()
+      }
+    }
+    var validatePhonenumber = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('电话号码不能为空'))
+      } else if (!(/^[1][3,4,5,7,8][0-9]{9}$/.test(value))) {
+        callback(new Error('请输入正确的电话号码格式!'))
+      } else {
+        callback()
+      }
+    }
     return {
-      username: '',
-      password: '',
-      email: '',
-      phonenumber: '',
-      secondpassword: '',
-      username_valid: false,
-      username_input: false,
-      passwordcheck: false,
-      password_input: false,
-      email_valid: false,
-      email_input: false,
-      phonenumber_valid: false,
-      phonenumber_input: false
-    }
-  },
-  watch: {
-  // 检测用户输入是否合法
-    username: {
-      handler (newName) {
-        this.username_valid = /^[A-Za-z\u4e00-\u9fa5][-A-Za-z0-9\u4e00-\u9fa5_]*$/.test(newName)
-        this.username_input = (this.username !== '')
-      }
-    },
-    secondpassword: {
-      handler () {
-        this.passwordcheck = (this.password === this.secondpassword && this.password !== '')
-        this.password_input = (this.secondpassword !== '')
-      }
-    },
-    password: {
-      handler () {
-        this.passwordcheck = (this.password === this.secondpassword && this.password !== '')
-      }
-    },
-    email: {
-      handler (newEmail) {
-        this.email_valid = /^([a-zA-Z0-9]+[_|_|.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|_|.]?)*[a-zA-Z0-9]+.[a-zA-Z]{2,3}$/.test(newEmail)
-        this.email_input = (this.email !== '')
-      }
-    },
-    phonenumber: {
-      handler (newPhoneNumber) {
-        this.phonenumber_valid = /^[1][3,4,5,7,8][0-9]{9}$/.test(newPhoneNumber)
-        this.phonenumber_input = (this.phonenumber !== '')
+      ruleForm: {
+        username: '',
+        password: '',
+        email: '',
+        phonenumber: '',
+        passwordCheck: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 1, max: 20, message: '长度在 1 到 20 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入用户密码', trigger: 'blur' },
+          { min: 1, max: 20, message: '长度在 1 到 20 个字符', trigger: 'blur' }
+        ],
+        passwordCheck: [
+          { validator: validatePass, trigger: 'blur' }
+        ],
+        email: [
+          { validator: validateEmail, trigger: 'blur' }
+        ],
+        phonenumber: [
+          { validator: validatePhonenumber, trigger: 'blur' }
+        ]
       }
     }
   },
-  // 得出是否能够提交
-  computed: {
-    total_valid: function () {
-      return this.username_valid && this.phonenumber_valid && this.email_valid && this.passwordcheck
+  methods: {
+    submitForm (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!!')
+          // 请在这里实现数据合理时的向后端发送请求，也可以将上一行删掉
+        } else {
+          return false
+        }
+      })
     }
   }
 }
--- frontend/tests/unit/Register.spec.js
+++ frontend/tests/unit/Register.spec.js
@@ -5,11 +5,11 @@ describe('Register.vue', () => {
   it('renders correctly with these inputs', () => {
     const Constructor = Vue.extend(Register);
     const RegisterComponent = new Constructor().$mount();
-    RegisterComponent.username = 'rzotgorz'
-    RegisterComponent.password = '123456'
-    RegisterComponent.secondpassword = '123456'
-    RegisterComponent.email = '123456@qq.com'
-    RegisterComponent.phonenumber = '18725846587'
+    RegisterComponent.ruleForm.username = 'rzotgorz'
+    RegisterComponent.ruleForm.password = '123456'
+    RegisterComponent.ruleForm.secondpassword = '123456'
+    RegisterComponent.ruleForm.email = '123456@qq.com'
+    RegisterComponent.ruleForm.phonenumber = '18725846587'
     const button = RegisterComponent.$el.querySelector('el-button');
     const clickEvent = new window.Event('click');
     button.dispatchEvent(clickEvent);

71fb17f278723279887e3ac57fd573f64211165d
hjq <huajq19@mails.tsinghua.edu.cn>
2020-10-16 22:25:16 +0800

    add search router and add searchresult page and search component#15

---
+++ frontend/src/components/Search.vue
@@ -0,0 +1,22 @@
+<template>
+<div style="padding:  1rem;" class="news">
+    {{keyword}}
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  data () {
+    return {
+      keyword: ''
+    }
+  },
+  mounted () {
+    this.keyword = this.$route.params.keyword
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
---
+++ frontend/src/views/SearchResult.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="SearchResult" >
+      <Search/>
+  </div>
+</template>
+
+<script>
+import Search from '@/components/Search.vue'
+
+export default {
+  name: 'SearchResult',
+  components: {
+    Search
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>

f23e61362a7d67e0d2758471d321aa25e3d682fd
wwh <wuwh19@mails.tsinghua.edu.cn>
2020-10-22 09:55:48 +0800

    add the database part #22

--- config/local_settings.py
+++ config/local_settings.py
@@ -9,18 +9,18 @@ DEBUG = False
 
 CORS_ORIGIN_ALLOW_ALL = False
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'news',
-#        'USER': 'postgres',
-#        'PASSWORD': '12345678',
-#        'HOST': 'postgres.rzotgorz.secoder.local',
-#        'PORT': '5432',
-#        # 'TEST': {
-#        #     'NAME': 'news'
-#        # }
-#    }
-# }
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'news',
+       'USER': 'postgres',
+       'PASSWORD': '12345678',
+       'HOST': 'postgres.rzotgorz.secoder.local',
+       'PORT': '5432',
+       # 'TEST': {
+       #     'NAME': 'news'
+       # }
+   }
+}
 
 STATICFILES_DIR = os.path.join(BASE_DIR, 'frontend', 'dist')

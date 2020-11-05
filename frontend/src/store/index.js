84006fd4daacab5ec6ce55fe54d8f211e743936f
Ren <ryy19@mails.tsinghua.edu.cn>
2020-11-05 14:38:04 +0800

    Change the interceptor to avoid check option #34

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,18 +5,22 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    token: ''
+    token: '',
+    username: ''
   },
   mutations: {
     // 保存口令
-    set_token (state, token) {
-      state.token = token
-      sessionStorage.token = token
+    set_token (state, data) {
+      state.token = data.token
+      state.username = data.username
+      sessionStorage.token = data.token
+      sessionStorage.username = data.username
     },
     // 删除口令
     rm_token (state) {
       state.token = ''
       sessionStorage.removeItem('token')
+      sessionStorage.removeItem('username')
     }
   }
 })
